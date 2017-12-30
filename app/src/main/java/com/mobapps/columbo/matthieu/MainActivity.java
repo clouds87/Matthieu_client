0c7da722dfc4e216940b4de8e8d3e33320bbb767
lorenzo.columbo <lorenzo.columbo87@gmail.com>
2017-12-30 19:01:14 +0100

    Changed GUI for buttons based control of boat speed.

--- app/src/main/java/com/mobapps/columbo/matthieu/MainActivity.java
+++ app/src/main/java/com/mobapps/columbo/matthieu/MainActivity.java
@@ -16,13 +16,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ToggleButton;
-import android.widget.SeekBar;
 
 import java.io.DataOutputStream;
 import java.net.Socket;
 
-// ToDo: verify functionality of periodically sending information about how to drive the boat, e.g. "\D7L3/"
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private final String MATTHIEU_SERVER_IP_ADDR = "192.168.4.1";
@@ -30,21 +27,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private final int UPDATE_PERIOD_MS = 100;
 
     private Button connectBtn;
-    private SeekBar speedCtrl;
-    private ToggleButton brakeCtrl;
-    private ToggleButton backwardCtrl;
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
 
     private Handler mainHandler;
-    private Handler networkHandler;
 
+    // Member variables for the communication from network thread towards main (GUI) thread
     private boolean connectReq = false;
     private boolean connectAck = false;
-    private int speed = 0;
-    private boolean brake = false;
-    private boolean backward = false;
-    private float accY = 0;
+
+    // Member variables for the communication from main (GUI) thread towards network thread
+    private boolean boatCmdFlag = false;
+    private int boatDirection = 0;
+    private int boatSpeed = 0;
 
     private Socket clientSocket;
     private DataOutputStream outToServer;
@@ -69,39 +64,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         });
 
-        // Define a seekbar to allow user specify a speed to drive the boat
-        speedCtrl = (SeekBar) findViewById(R.id.speed_ctrl);
-        speedCtrl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                speed = progress;
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
-
-        // Define a toggle button to allow the user stop the boat
-        brakeCtrl = (ToggleButton) findViewById(R.id.brake_ctrl);
-        brakeCtrl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                brake = brakeCtrl.isChecked();
-            }
-        });
-
-        // Define a toggle button to allow the user make the boat go backward
-        backwardCtrl = (ToggleButton) findViewById(R.id.backward_ctrl);
-        backwardCtrl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                backward = backwardCtrl.isChecked();
-            }
-        });
-
         // Define a sensor manager and an accelerometer sensor to let the user control the steering
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -122,6 +84,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public final void onSensorChanged(SensorEvent event) {
         // Many sensors return 3 values, one for each axis.
+        // TODO: set boatDirection and enable boatCmdFlag
+        float accY;
         accY = event.values[1];
     }
 
@@ -140,28 +104,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     };
 
-    // Define a runnable periodically sending a string which contains all the control information needed to drive the boat
+    // Define a runnable periodically checking for connection and sending a string which contains
+    // all the control information needed to drive the boat
     private Runnable networkUpdate = new Runnable() {
         @Override
         public void run() {
+            String updateString;
             while(true) {
                 if(connectReq) {
                     if(connectAck) {
-                        String updateString;
-                        int absAccY = 0;
+                        // TODO: build and send update string only if needed
                         updateString = "";
                         updateString = updateString + "\\";
-                        if(brakeCtrl.isChecked())
-                            updateString = updateString + "B";
-                        else
-                        if(backwardCtrl.isChecked())
-                            updateString = updateString + "R";
-                        else
-                            updateString = updateString + "D";
-                        updateString = updateString + Integer.toString(speedCtrl.getProgress()/10);
-                        updateString = updateString + (accY > 0 ? "R" : "L");
-                        absAccY = Math.abs(Math.round(accY));
-                        updateString = updateString + ((absAccY > 9) ? 9 : absAccY);
+                        updateString = updateString + "A0D0";
                         updateString = updateString + ("/");
                         try {
                             outToServer.writeBytes(updateString);
@@ -197,92 +152,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     };
     private Thread networkThread = new Thread(networkUpdate);
 
-    /*
-    private Socket clientSocket;
-    private DataOutputStream outToServer;
-    AlertDialog alertDialog;
-
-    private Handler mainHandler;
-    private Handler connectHandler;
-
-    private float accY = 0;
-
-    private Runnable sendUpdate = new Runnable(){
-        @Override
-        public void run() {
-            String updateString;
-            int absAccY = 0;
-            Log.d("PROGRESS", String.format("%d",speedCtrl.getProgress()/10));
-            Log.d("ACC_Y", String.format("%.2f",accY));
-            updateHandler.postDelayed(sendUpdate, UPDATE_PERIOD_MS);
-            if (clientSocket != null && clientSocket.isConnected()) {
-                updateString = "";
-                updateString = updateString + "\\";
-                if(brakeCtrl.isChecked())
-                    updateString = updateString + "B";
-                else
-                if(backwardCtrl.isChecked())
-                    updateString = updateString + "R";
-                else
-                    updateString = updateString + "D";
-                updateString = updateString + Integer.toString(speedCtrl.getProgress()/10);
-                updateString = updateString + (accY > 0 ? "R" : "L");
-                absAccY = Math.abs(Math.round(accY));
-                updateString = updateString + ((absAccY > 9) ? 9 : absAccY);
-                updateString = updateString + ("/");
-                try {
-//                    outToServer.writeBytes(updateString);
-                    Log.d("UD_MSG", updateString);
-                } catch (Exception e) {
-                    Log.d("EXC_ERR",e.getMessage());
-                }
-            }
-       }
-    };
-
-    private Thread connectThread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            if (clientSocket == null || !clientSocket.isConnected()) {
-                try {
-                    clientSocket = new Socket(MATTHIEU_SERVER_IP_ADDR, MATTHIEU_SERVER_IP_PORT);
-                    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-                    connectBtn.setBackgroundColor(Color.GREEN);
-                } catch(Exception e) {
-                    alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-                    alertDialog.setTitle("Connection error");
-                    alertDialog.setMessage("Couldn't connect to Matthieu server (" + MATTHIEU_SERVER_IP_ADDR + ":" + MATTHIEU_SERVER_IP_PORT + "). " +
-                            "Please check that the correct WiFi AP is being used and re-try.");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            });
-                    alertDialog.show();
-                }
-            } else {
-                try {
-                    clientSocket.close();
-                    connectBtn.setBackgroundColor(Color.RED);
-                } catch(Exception e) {
-                    alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-                    alertDialog.setTitle("Connection error");
-                    alertDialog.setMessage("Couldn't disconnect from Matthieu server (" + MATTHIEU_SERVER_IP_ADDR + ":" + MATTHIEU_SERVER_IP_PORT + "). " +
-                            "Please restart both client and server applications.");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            });
-                    alertDialog.show();
-                }
-            }
-        }
-    });
-
-*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -298,7 +167,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
+            // TODO: add a simple dialog with app name, version and authors
         }
 
         return super.onOptionsItemSelected(item);
